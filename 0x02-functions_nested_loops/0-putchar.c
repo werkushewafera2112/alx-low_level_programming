1dc6671e7d190528c264e2fc8ae5aaea2c02ee02
werkushewafera2112 <werkushewafera2112@gmail.com>
2022-09-13 21:32:18 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "werku.h"
+/**
+ * main - Prints werku
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('w');
+	_putchar('e');
+	_putchar('r');
+	_putchar('k');
+	_putchar('u');
+	return (0);
