9ae39e51a7239c3bdfb6ebff0c9ddc9ebce83f49
werkushewafera2112 <werkushewafera2112@gmail.com>
2022-09-19 16:51:59 +0300

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,22 +7,9 @@
 
 void swap_int(int *a, int *b)
 {
-	int temp;
+        int temp;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+        temp = *a;
+        *a = *b;
+        *b = temp;
 }
