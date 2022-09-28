4ef7a675e5cf92ff991e29dadb9d36374380e702
werkushewafera2112 <werkushewafera2112@gmail.com>
2022-09-28 11:34:36 +0300

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
+#include "main.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
 /**
-  * checker - checks recursively the input from is_prime_number
-  * @n: iterator
-  * @base: base number to check
-  * Return: 1 if n is a prime, else return 0 otherwise.
-  */
-int checker(int n, int base)
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
 {
-	if (base % n == 0 || base < 2)
+	if (num % div == 0)
 		return (0);
-	else if (n == base - 1)
+
+	if (div == num / 2)
 		return (1);
-	else if (base > n)
-		return (checker(n + 1, base));
-	return (1);
+
+	return (is_divisible(num, div + 1));
 }
+
 /**
-  * is_prime_number - checks if the number is a prime number
-  * @n: the number to check
-  * Return: 1 if n is a prime, else return 0 otherwise.
-  */
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
+ */
 int is_prime_number(int n)
 {
-	return (checker(2, n));
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
 }
