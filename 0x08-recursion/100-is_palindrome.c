bc9bd9b3a2554a7827346009e778abfbd1e00c0f
werkushewafera2112 <werkushewafera2112@gmail.com>
2022-09-28 11:19:25 +0300

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,62 @@
 #include "main.h"
 
-/**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int palind2(char *a, int l)
-{
-	if (*a == 0)
-		return (l - 1);
-	return (palind2(a + 1, l + 1));
-}
-/**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
-int palind3(char *a, int l)
-{
-	if (*a != *(a + l))
-		return (0);
-	else if (*a == 0)
-		return (1);
-	return (palind3(a + 1, l - 2));
-}
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
 int is_palindrome(char *s)
 {
-	int l;
+	int index = 0;
+	int len = find_strlen(s);
 
-	l = palind2(s, 0);
-	return (palind3(s, l));
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
 }
